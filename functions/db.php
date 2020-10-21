db960f604162d151401116057069aef47cf3b09e
Christopher Edynak <christopher1edynak@gmail.com>
2020-10-21 13:02:56 -0400

    implemented db helper functions

--- functions/db.php
+++ functions/db.php
@@ -1,3 +1,25 @@
 <?php 
     $con = mysqli_connect('localhost', 'root', '', 'simple-login-registration');
+
+    function escape($string) {
+        global $con;
+        return mysqli_real_escape_string($con, $string); 
+    }
+
+    function query($query){
+        global $con;
+        return mysqli_query($con, $query);
+    }
+
+    function confirm($result){
+        global $con;
+        if(!$result){
+            die("QUERY FAILED", mysqli_error($con));
+        }
+    }
+
+    function fetch_array($result){
+        global $con;
+        return mysqli_fetch_array($result);
+    }
 ?>
